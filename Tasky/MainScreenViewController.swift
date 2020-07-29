2d63cd48ef695c53292b518d37c5e100fa0bf13b
Indra-k <indra.yao1999@gmail.com>
2020-07-29 11:46:34 +0700

    initial commit

---
+++ Tasky/MainScreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainScreenViewController.swift
+//  Tasky
+//
+//  Created by Indra Kurniawan on 29/07/20.
+//  Copyright © 2020 Indra Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func selectWorkIntervalPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func workIntervalPressed(_ sender: UIButton) {
+    }
+    
+}
---
+++ Tasky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasky
+//
+//  Created by Indra Kurniawan on 28/07/20.
+//  Copyright © 2020 Indra Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

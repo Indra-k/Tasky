4493b04f21a90a4bb11dc8f38cead13c34252c12
Indra-k <indra.yao1999@gmail.com>
2020-07-29 13:20:02 +0700

    add img asset, new constrain, picker

--- Tasky/StartViewController.swift
+++ Tasky/StartViewController.swift
@@ -8,23 +8,30 @@
 
 import UIKit
 
-class StartViewController: UIViewController {
-
+class StartViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
+    
+    @IBOutlet var workPicker: UIPickerView!
+    
+    let data = ["0", "1", "2", "3", "4", "5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        workPicker.dataSource = self
+        workPicker.delegate = self
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+

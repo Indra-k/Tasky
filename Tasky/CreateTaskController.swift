757249a7cb6067319772f36e42b6bdfd3e7aeea0
braannn <syabran99@gmail.com>
2020-08-06 02:18:24 +0700

    add StartTaskController.swft and screen, add CreateTaskController for create task screen

---
+++ Tasky/CreateTaskController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateTaskController.swift
+//  Tasky
+//
+//  Created by Muhammad Syabran on 06/08/20.
+//  Copyright © 2020 Indra Kurniawan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateTaskController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Tasky/StartTaskController.swift
@@ -0,0 +1,18 @@
+//
+//  StartTaskController.swift
+//  Tasky
+//
+//  Created by Muhammad Syabran on 06/08/20.
+//  Copyright © 2020 Indra Kurniawan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartTaskController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

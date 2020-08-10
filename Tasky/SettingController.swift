7605ad14f715d552c4e9bf005abcbb755e639380
Indra-k <indra.yao1999@gmail.com>
2020-08-10 21:16:58 +0700

    tambah pickerButton untuk WorkTime, Interval dkk

--- Tasky/SettingController.swift
+++ Tasky/SettingController.swift
@@ -12,7 +12,13 @@ class SettingController: UIViewController,UIPickerViewDataSource, UIPickerViewDe
 
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var workIntervalBtn: UIButton!
+    @IBOutlet weak var workTimeBtn: UIButton!
+    @IBOutlet weak var shortBreakBtn: UIButton!
+    @IBOutlet weak var longBreakBtn: UIButton!
+    @IBOutlet weak var longBreakAfterBtn: UIButton!
     
+    var selectedButton = UIButton()
+    var selectedList : [String] = []
     let workInterval = ["1", "2", "3", "4", "5" ,"6" ,"7" ,"8" ,"9" ,"10" ,"11" ,"12" ,"13" ,"14" ,"15" ,"16"]
     let workTime = ["5" ,"10" ,"15" ,"20" ,"25" ,"30" ,"35" ,"40" ,"55" ,"60"]
     let shortBreak = ["1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9" ,"10" ,"15" ,"20" ,"25" ,"30"]
@@ -20,13 +26,12 @@ class SettingController: UIViewController,UIPickerViewDataSource, UIPickerViewDe
     let longBreakAfter = ["2 Interval", "3 Interval", "4 Interval", "5 Interval", "6 Interval", "7 Interval", "8 Interval"]
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         pickerView.isHidden = true
-        
         pickerView.delegate = self
         pickerView.dataSource = self
         
-        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
@@ -35,6 +40,59 @@ class SettingController: UIViewController,UIPickerViewDataSource, UIPickerViewDe
         if pickerView.isHidden {
             pickerView.isHidden = false
         }
+        selectedButton = workIntervalBtn
+        selectedList = workInterval
+        print(selectedList[0])
+        
+        pickerView.reloadAllComponents()
+    }
+    
+    //MARK: - Work Time
+    @IBAction func workTimePressed(_ sender: UIButton) {
+        if pickerView.isHidden {
+            pickerView.isHidden = false
+        }
+        selectedButton = workTimeBtn
+        selectedList = workTime
+        print(selectedList[0])
+        
+        pickerView.reloadAllComponents()
+    }
+    
+    //MARK: - Short Break
+    @IBAction func shortBreakPressed(_ sender: UIButton) {
+        if pickerView.isHidden {
+            pickerView.isHidden = false
+        }
+        selectedButton = shortBreakBtn
+        selectedList = shortBreak
+        print(selectedList[0])
+        
+        pickerView.reloadAllComponents()
+    }
+    
+    //MARK: - Long Break
+    @IBAction func longBreakPressed(_ sender: UIButton) {
+        if pickerView.isHidden {
+            pickerView.isHidden = false
+        }
+        selectedButton = longBreakBtn
+        selectedList = longBreak
+        print(selectedList[0])
+        
+        pickerView.reloadAllComponents()
+    }
+    
+    //MARK: - Long Break After
+    @IBAction func longBreakAfterPressed(_ sender: UIButton) {
+        if pickerView.isHidden {
+            pickerView.isHidden = false
+        }
+        selectedButton = longBreakAfterBtn
+        selectedList = longBreakAfter
+        print(selectedList[0])
+        
+        pickerView.reloadAllComponents()
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -42,16 +100,18 @@ class SettingController: UIViewController,UIPickerViewDataSource, UIPickerViewDe
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return workInterval.count
+        return selectedList.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return workInterval[row]
+        return selectedList[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        workIntervalBtn.setTitle(workInterval[row], for: .normal)
+        selectedButton.setTitle(selectedList[row], for: .normal)
         pickerView.isHidden = true
     }
     
 }
+
+
